36137dacaabced31bbc5fb602e394ff95a887cb6
Sia819 <jjn970819@gmail.com>
2021-01-26 23:26:25 +0900

    2021.01.26 Init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # WindowTitleControl
+
+This project is still developing it make may occur errors.
---
+++ WindowTitleControl/WindowTitleControl/CostomTitle.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowTitleControl
+{
+    /// <summary>
+    /// CostomTitle.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class CostomTitle : UserControl
+    {
+        public CostomTitle()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources.Add("myBrush", new SolidColorBrush(Color.FromRgb(255, 0, 0)));
+        }
+    }
+}
